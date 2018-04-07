5e6c0aeaaeff11de2ece35d53b52430bfaf10554
AlexAkulov <alexakulov86@gmail.com>
2018-04-07 23:05:07 +0500

    random generation

--- Assets/Game.cs
+++ Assets/Game.cs
@@ -11,11 +11,14 @@ public class Game : MonoBehaviour
 
 	public GameObject Web;
 	public GameObject Chest;
-	
+	public GameObject Torch;
+	public GameObject Rat;
 	
 	private const int NumberOfRooms = 5;
 	private const int NumberOfWeb = 10;
 	private const int NumberOfChests = 5;
+	private const int NumberOfTorch = 5;
+	private const int NumberOfEnemies = 5;
 	
 	
 	private const int Height = 31;
@@ -27,7 +30,9 @@ public class Game : MonoBehaviour
 	private struct Room
 	{
 		public GameObject web;
-		public GameObject chast;
+		public GameObject chest;
+		public GameObject torch;
+		public GameObject Enemy;
 	}
 	
 	private Room[,] Level = new Room[NumberOfRooms,NumberOfRooms];
@@ -62,23 +67,55 @@ public class Game : MonoBehaviour
 
 	private void generateChest()
 	{
-		for (var i = 0; i < NumberOfWeb; i++)
+		for (var i = 0; i < NumberOfChests; i++)
 		{
 			var x = UnityEngine.Random.RandomRange(0, NumberOfRooms);
 			var y = UnityEngine.Random.RandomRange(0, NumberOfRooms);
 			
-			var w = Instantiate(Chest);
+			var c = Instantiate(Chest);
 		
-			w.transform.position = new Vector3(x*Width + Left, y*Height + Top, 0);
-			Level[x,y].web = w;
+			c.transform.position = new Vector3(x*Width + Left, y*Height + Top- 12, 0);
+			Level[x,y].chest = c;
+		}
+	}
+
+	private void generateTorch()
+	{
+		for (var i = 0; i < NumberOfTorch; i++)
+		{
+			var x = UnityEngine.Random.RandomRange(0, NumberOfRooms);
+			var y = UnityEngine.Random.RandomRange(0, NumberOfRooms);
+			
+			var t = Instantiate(Torch);
+		
+			t.transform.position = new Vector3(x*Width + Left, y*Height + Top, 0);
+			Level[x,y].torch = t;
 
 			print(string.Format("create web on x: {0} y: {1}", x, y));
 		}
 	}
+	
+	private void generateEnemies()
+	{
+		for (var i = 0; i < NumberOfEnemies; i++)
+		{
+			var x = UnityEngine.Random.RandomRange(0, NumberOfRooms);
+			var y = UnityEngine.Random.RandomRange(0, NumberOfRooms);
+			
+			var t = Instantiate(Rat);
+		
+			t.transform.position = new Vector3(x*Width + Left + 16, y*Height + Top - 11, 0);
+			Level[x,y].Enemy = t;
 
+			print(string.Format("create web on x: {0} y: {1}", x, y));
+		}
+	}
+	
 	private void BuildLevel()
 	{
 		generateWeb();
 		generateChest();
+		generateTorch();
+		generateEnemies();
 	}
 }
