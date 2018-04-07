bf9f31080952b22b2948b022d833c5628e896a59
densnz <densnz8@gmail.com>
2018-04-07 22:13:38 +0500

    fix move between rooms

--- Assets/doorMove.cs
+++ Assets/doorMove.cs
@@ -1,49 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+public static class TargetState
+{
+    static TargetState()
+    {
+        IsInited = false;
+    }
+
+    public static bool IsInited { get; set; }
+    public static Vector3 Target { get; set; }
+}
+
 public class doorMove : MonoBehaviour, IPointerClickHandler
 {
     public new GameObject camera;
     public GameObject nextRoom;
-    private  int speed = 10;
 
-
-    private static readonly Vector3 fakePosition = new Vector3(-2000, -2000, -2000);
-    private Vector3 position = fakePosition;
-
-
-    private int i = 0;
     // Use this for initialization
-    void Start ()
+    public void Start ()
     {
     }
     
     // Update is called once per frame
-    void Update ()
+    public void Update ()
     {
-        if (position != fakePosition)
+        if (TargetState.IsInited)
         {
-            //print ("123");
-            // if (i++ % 10000 == 0)
-            // {
-            //     print("321");
-            //     // print(camera.transform.position);
-            // }
+            float step = 8 * Time.deltaTime;
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, TargetState.Target, step);
 
-            float step = speed * Time.deltaTime;
-            camera.transform.position += new Vector3(step, 0f, -1);
-            // camera.transform.position = Vector3.MoveTowards(camera.transform.position, position, step);
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        var transformPosition = nextRoom.transform.position - new Vector3(0, 0, 1);
-        position = transformPosition;
-        //print(position);
-        //camera.transform.position = transformPosition;
-        //hero.transform.position = nextRoom.transform.position;
-
+        TargetState.Target = nextRoom.transform.position - new Vector3(0, 0, 1);
+        TargetState.IsInited = true;
         var room = nextRoom.GetComponent<room>();
         room.EnterRoom();
     }
