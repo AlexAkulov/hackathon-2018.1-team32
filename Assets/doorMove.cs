c7abdb22f78a0516c329236d983228af23247926
densnz <densnz8@gmail.com>
2018-04-07 18:00:33 +0500

    add up/down doors

--- Assets/doorMove.cs
+++ Assets/doorMove.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class doorMove : MonoBehaviour {
+public class doorMove : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject camera;
+    public GameObject nextRoom;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public static int offsetX = 60;
+    public static int offsetY = 31;
+
+    // Use this for initialization
+    void Start ()
+    {
+    }
+    
+    // Update is called once per frame
+    void Update ()
+    {
+        
+    }
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print ("123");
+        var transformPosition = nextRoom.transform.position - new Vector3(0, 0, 1);
+        camera.transform.position = transformPosition;
+        //hero.transform.position = nextRoom.transform.position;
+
+        var room = nextRoom.GetComponent<room>();
+        room.EnterRoom();
+
+    }
 }
