c13e8ff4b306824761d4d593181847f343b843d9
Alexey Ivanov <alexei.nt@gmail.com>
2018-04-07 21:56:09 +0500

    Smoothed moving

--- Assets/doorMove.cs
+++ Assets/doorMove.cs
@@ -1,46 +1,42 @@
 ﻿using UnityEngine;
 using UnityEngine.EventSystems;
 
+public static class TargetState
+{
+    static TargetState()
+    {
+        IsInited = false;
+    }
+
+    public static bool IsInited { get; set; }
+    public static Vector3 Target { get; set; }
+}
+
 public class doorMove : MonoBehaviour, IPointerClickHandler
 {
     public new GameObject camera;
     public GameObject nextRoom;
-    private  int speed = 100;
 
-
-    private static readonly Vector3 fakePosition = new Vector3(-1, -1, -1);
-    private Vector3 position = fakePosition;
-
-
-    private int i = 0;
     // Use this for initialization
-    void Start ()
+    public void Start ()
     {
     }
     
     // Update is called once per frame
-    void Update ()
+    public void Update ()
     {
-        if (position != fakePosition)
+        if (TargetState.IsInited)
         {
-            if (i++ % 10000 == 0)
-            {
-                print(position);
-                print(camera.transform.position);
-            }
+            float step = 2 * Time.deltaTime;
+            camera.transform.position = Vector3.MoveTowards(camera.transform.position, TargetState.Target, step);
 
-            float step = speed * Time.deltaTime;
-            camera.transform.position = Vector3.MoveTowards(camera.transform.position, position, step);
         }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        var transformPosition = nextRoom.transform.position - new Vector3(0, 0, 1);
-        position = transformPosition;
-        //print(position);
-        //camera.transform.position = transformPosition;
-        //hero.transform.position = nextRoom.transform.position;
+        TargetState.Target = nextRoom.transform.position - new Vector3(0, 0, 1);
+        TargetState.IsInited = true;
 
         var room = nextRoom.GetComponent<room>();
         room.EnterRoom();
