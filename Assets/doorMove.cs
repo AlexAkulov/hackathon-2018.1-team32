7de8bf9801f1cdc5a738dde226ca8b8eb47a5b18
Alexey Ivanov <alexei.nt@gmail.com>
2018-04-07 16:45:12 +0500

    Logic experiments

--- Assets/doorMove.cs
+++ Assets/doorMove.cs
@@ -1,16 +1,44 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class doorMove : MonoBehaviour {
+public class doorMove : MonoBehaviour, IPointerClickHandler
+{
+    public GameObject camera;
+    public GameObject nextRoom;
+    public GameObject hero;
 
-	// Use this for initialization
-	void Start () {
-		
+    public static int offsetX = 60;
+    public static int offsetY = 31;
+    private Renderer renderer;
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D boxCollider2D;
+
+    // Use this for initialization
+	void Start ()
+	{
+	    renderer = GetComponent<Renderer>();
+	    rigidbody = GetComponent<Rigidbody2D>();
+	    boxCollider2D = GetComponent<BoxCollider2D>();
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+    {
+
+
+        
 	}
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        var transformPosition = nextRoom.transform.position - new Vector3(0, 0, 1);
+        camera.transform.position = transformPosition;
+        //hero.transform.position = nextRoom.transform.position;
+
+        var room = nextRoom.GetComponent<room>();
+        room.EnterRoom();
+
+        Debug.Log("AHDKFLJDSKLF:HJH:SDLKJHFLKSJDHFKJDSHF");
+
+    }
 }
---
+++ Assets/room.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class room : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void EnterRoom()
+    {
+        print("Room entered");
+    }
+}
