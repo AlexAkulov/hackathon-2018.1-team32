9b7adae4d44246e4a748905b62ce15e4d300715b
densnz <densnz8@gmail.com>
2018-04-07 22:03:56 +0500

    gui panel

--- Assets/doorMove.cs
+++ Assets/doorMove.cs
@@ -3,12 +3,16 @@ using UnityEngine.EventSystems;
 
 public class doorMove : MonoBehaviour, IPointerClickHandler
 {
-    public GameObject camera;
+    public new GameObject camera;
     public GameObject nextRoom;
+    private  int speed = 10;
 
-    public static int offsetX = 60;
-    public static int offsetY = 31;
 
+    private static readonly Vector3 fakePosition = new Vector3(-2000, -2000, -2000);
+    private Vector3 position = fakePosition;
+
+
+    private int i = 0;
     // Use this for initialization
     void Start ()
     {
@@ -17,18 +21,30 @@ public class doorMove : MonoBehaviour, IPointerClickHandler
     // Update is called once per frame
     void Update ()
     {
-        
+        if (position != fakePosition)
+        {
+            //print ("123");
+            // if (i++ % 10000 == 0)
+            // {
+            //     print("321");
+            //     // print(camera.transform.position);
+            // }
+
+            float step = speed * Time.deltaTime;
+            camera.transform.position += new Vector3(step, 0f, -1);
+            // camera.transform.position = Vector3.MoveTowards(camera.transform.position, position, step);
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        print ("123");
         var transformPosition = nextRoom.transform.position - new Vector3(0, 0, 1);
-        camera.transform.position = transformPosition;
+        position = transformPosition;
+        //print(position);
+        //camera.transform.position = transformPosition;
         //hero.transform.position = nextRoom.transform.position;
 
         var room = nextRoom.GetComponent<room>();
         room.EnterRoom();
-
     }
 }
