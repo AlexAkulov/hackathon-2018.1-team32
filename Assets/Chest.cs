d62798f0e1e2d6cd96368942664531f6f255b7f4
AlexAkulov <alexakulov86@gmail.com>
2018-04-08 01:54:23 +0500

    Add chest disappear

---
+++ Assets/Chest.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Chest : MonoBehaviour, IPointerClickHandler {
+
+	
+	SpriteRenderer m_SpriteRenderer;
+	private bool destroyed;
+	static float t = 0.0f;
+	
+	void Start () {
+		m_SpriteRenderer = GetComponent<SpriteRenderer>();	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (destroyed)
+		{
+			t += 3 * Time.deltaTime;
+			var a = Mathf.Lerp(1.0f, 0.0f, t);
+			m_SpriteRenderer.color = new Color(m_SpriteRenderer.color.r, m_SpriteRenderer.color.g, m_SpriteRenderer.color.b, a);
+			if (a == 0)
+			{
+				Destroy(gameObject);		
+			}  
+		}		
+	}
+
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		destroyed = true;
+		var g = GameObject.Find("Game");
+		
+	}
+}
